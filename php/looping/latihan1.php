ca2feed416321f603a1c35ef9d6ee854b9f4430f
ranggaggngntt <ranggaakb48@yahoo.com>
2023-12-21 05:05:25 -0800

    looping latihan1 task5

--- php/looping/latihan1.php
+++ php/looping/latihan1.php
@@ -1,11 +1,10 @@
 <?php
 
-echo '<h1> Menggunakan perulangan While </h1>';
+echo '<h1> Menggunakan perulangan Do While </h1>';
 
-$i = 0;
-while ($i < 5) {
-    echo "Hai, ini baris ke-$i <br>";
-    $i++;
-}
+$i = 10;
+do {
+    echo "Hello world! <br>";
+} while ($i < 5);
 
 ?>
