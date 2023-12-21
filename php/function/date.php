a670da7e5091bf86b33e227dc6051fa70706580c
ranggaggngntt <ranggaakb48@yahoo.com>
2023-12-21 05:24:14 -0800

    function date task2

--- php/function/date.php
+++ php/function/date.php
@@ -1,5 +1,21 @@
 <?php
 
-echo date('l');
-
+$todayDate = "30 nov 2023";
+$returnDate = date('d-m-Y', strtotime('+100 day', strtotime($todayDate)));
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>
+        Tanggal hari ini = <?= $todayDate; ?>
+        <br>
+        Tanggal 100 Hari dari sekarang = <?= $returnDate; ?>
+    </h3>
+</body>
+</html>
