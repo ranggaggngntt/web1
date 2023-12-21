df93c911432b2b984135ac22d05721012f0bb030
ranggaggngntt <ranggaakb48@yahoo.com>
2023-12-21 05:26:00 -0800

    function date task4

--- php/function/date.php
+++ php/function/date.php
@@ -1,7 +1,7 @@
 <?php
-
-$todayDate = "30 nov 2023";
-$returnDate = date('d-m-Y', strtotime('+1000 day', strtotime($todayDate)));
+$todayDate = date('d M Y'); // Get the current date
+$futureDate = date('d-m-Y', strtotime('+1000 day', strtotime($todayDate)));
+$pastDate = date('d-m-Y', strtotime('-500 day', time()));
 ?>
 
 <!DOCTYPE html>
@@ -15,7 +15,9 @@ $returnDate = date('d-m-Y', strtotime('+1000 day', strtotime($todayDate)));
     <h3>
         Tanggal hari ini = <?= $todayDate; ?>
         <br>
-        Tanggal 1000 Hari dari sekarang = <?= $returnDate; ?>
+        Tanggal 1000 Hari dari sekarang = <?= $futureDate; ?>
+        <br>
+        Tanggal 500 Hari yang lalu = <?= $pastDate; ?>
     </h3>
 </body>
 </html>
